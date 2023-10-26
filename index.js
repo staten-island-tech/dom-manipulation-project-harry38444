09f166081b0eb28bcdb1747c8d7ce4250619f68e
harry38444 <harry38444@gmail.com>
2023-10-25 22:51:41 -0400

    q
    q

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 const DOMSelectors = {
     form: document.querySelector("#form"),
-    firstName: document.querySelector(".first-name"),
-    h2s:document.querySelectorAll("h2"),
+    h2s: document.querySelectorAll("h2"),
+    h3s: document.querySelectorAll("h3"),
+button: document.getElementById("button"),
+input: document.querySelector(`#input`),
+input2: document.querySelector(`#input2`),
+delete: document.querySelector(`#delete`)
 };
 
 
 DOMSelectors.form.addEventListener("submit", function(event){
    event.preventDefault();
-   console.log(DOMSelectors.firstName.value);
+   console.log(DOMSelectors.CardName.value);
    DOMSelectors.h2s.forEach(
-    (el)=> el.textContent = (DOMSelectors.firstName.value),
-    document.querySelector("Gallery")
-.insertAdjacentHTML(
-    "afterbegin",
-    `<div class="card"><h2 class="card-header">${DOMSelectors.firstName.value}</h2></div>`)
- );
-});
+    (el)=> el.textContent = (DOMSelectors.CardName.value),
+   )});
+   const card = DOMSelectors.button.addEventListener("click", function(){
+    let input= DOMSelectors.input.value;
+    let input2= DOMSelectors.input2.value;
+    DOMSelectors.form.insertAdjacentHTML("beforeend",
+    `<div class="column">
+    <div class="card">
+        <h2 class="card-header" id="column">${input}</h2>
+        <h3 class="card-info" id="column">${input2}</h3>
+    </div>
+    <button type="submit" id="delete">Delete</button>`);
+   DOMSelectors.input.value = "delete";
+   DOMSelectors.input2.value ="delete";
+   });
+
+ DOMSelectors.delete.addEventListener("submit", function(){
+    card.remove()
+ });
+ 
