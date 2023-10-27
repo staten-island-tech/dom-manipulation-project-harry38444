afc27065f5e6b215781461ccb3aa72cc2107cdc4
harry38444 <harry38444@gmail.com>
2023-10-27 10:19:33 -0400

    a
    a

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ input: document.querySelector(`#input`),
 input2: document.querySelector(`#input2`),
 delete: document.querySelector(`#delete`),
 form: document.querySelector("#form"),
+input3: document.querySelector(`#input3`),
     h2s: document.querySelectorAll("h2"),
     h3s: document.querySelectorAll("h3")
 };
@@ -19,19 +20,32 @@ DOMSelectors.form.addEventListener("submit", function(event){
    const card = DOMSelectors.button.addEventListener("click", function(){
     let input= DOMSelectors.input.value;
     let input2= DOMSelectors.input2.value;
+    let input3= DOMSelectors.input3.value;
     DOMSelectors.form.insertAdjacentHTML("beforeend",
     `<div class="column">
     <div class="card">
         <h2 class="card-header" id="column">${input2}</h2>
 
         <h3 class="card-info" id="column">${input}</h3>
+
+        <img src = "${input3}" alt = "" class = "img">
     </div>
     <button type="click" id="delete">Delete</button>`);
    DOMSelectors.input.value = "";
    DOMSelectors.input2.value ="";
+   DOMSelectors.input3.value ="";
    });
 
  DOMSelectors.delete.addEventListener("click", function(){
     card.remove()
+
+
  });
  
+ function remove () {
+    let btns = document .querySelectorAll("button")
+    btns.forEach((button)=> button.addEventListender('click', function (event) {
+        console.log(event.target.parentElement);
+    }))
+ }
+ remove();
